cf27604758b62df1a7d63713e6de88b2542db8bd
Jose Maria <jmgarcia549@gmail.com>
2024-04-13 13:16:17 +0200

    añadido ventanas js

---
+++ js/window.js
@@ -0,0 +1,7 @@
+function openWindow() {
+    document.getElementById("openWin").classList.add("view");
+};
+
+function closeWindow() {
+    document.getElementById("openWin").classList.remove("view");
+}
