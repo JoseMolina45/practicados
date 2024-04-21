4cb086bc1a5932dbdceaf2c571da8aec373e587d
Jose Maria <jmgarcia549@gmail.com>
2024-04-21 21:41:04 +0200

    Modificaciones responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
+
+//  Open main menu responsive
+function openMenu() {
+   document.getElementById("menu-click").classList.toggle("menu-show");   
+};
+
+// open window (div)
 function openWindow() {
     document.getElementById("openWin").classList.add("view");
 };
 
+// close window (div)
 function closeWindow() {
     document.getElementById("openWin").classList.remove("view");
-}
+};
